efad4f703368660171c6c3bfc6a1429daffac66a
DynastyElvis <79464171+DynastyElvis@users.noreply.github.com>
2022-03-07 09:16:58 +0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,5 @@
+//beginning of my js
+// variable declarations
 var CC, YY, MM, DD, d, dayValue;
 var dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi", "Kwame"];
